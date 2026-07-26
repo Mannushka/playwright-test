ff112d77d3bb95206fd07532a37aaa2d628fbd4b
Mannushka <zaimar1910@gmail.com>
2026-07-27 00:01:07 +0800

    test login for locked out user

--- tests/saucedemo-tests/login.spec.ts
+++ tests/saucedemo-tests/login.spec.ts
@@ -16,4 +16,15 @@ test("should log in a valid user", async ({ page }) => {
   await page.getByPlaceholder(/Username/).fill("standard_user");
   await page.getByPlaceholder(/Password/).fill("secret_sauce");
   await page.getByRole("button", { name: /Login/ }).click();
+  await expect(page).toHaveURL(/.*inventory.html/);
+});
+
+test("should not log in an invalid user", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/");
+  await page.getByPlaceholder(/Username/).fill("locked_out_user");
+  await page.getByPlaceholder(/Password/).fill("secret_sauce");
+  await page.getByRole("button", { name: /Login/ }).click();
+  await expect(
+    page.getByText("Epic sadface: Sorry, this user has been locked out."),
+  ).toBeVisible();
 });
