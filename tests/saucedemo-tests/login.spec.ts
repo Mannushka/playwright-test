369cc26f79023499262d134c20c044bccbd5aefb
Mannushka <zaimar1910@gmail.com>
2026-09-13 18:30:18 +0800

    fix user data import

--- tests/saucedemo-tests/login.spec.ts
+++ tests/saucedemo-tests/login.spec.ts
@@ -1,4 +1,11 @@
+import { UserDataFile, TestUser } from "../../types/user.types";
 import { test, expect } from "@playwright/test";
+import * as fs from "fs";
+import * as path from "path";
+
+const users: UserDataFile = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "../../data/users.json"), "utf-8"),
+);
 
 test("should load login page with title and form controls", async ({
   page,
@@ -11,20 +18,23 @@ test("should load login page with title and form controls", async ({
   await expect(page.getByRole("button", { name: /Login/ })).toBeVisible();
 });
 
-test("should log in a valid user", async ({ page }) => {
+test("should log in a valid user with correct credentials", async ({
+  page,
+}) => {
   await page.goto("https://www.saucedemo.com/");
-  await page.getByPlaceholder(/Username/).fill("standard_user");
-  await page.getByPlaceholder(/Password/).fill("secret_sauce");
+  await page.getByPlaceholder(/Username/).fill(users.validUsers[0].username);
+  await page.getByPlaceholder(/Password/).fill(users.validUsers[0].password);
   await page.getByRole("button", { name: /Login/ }).click();
   await expect(page).toHaveURL(/.*inventory.html/);
 });
 
 test("should not log in an invalid user", async ({ page }) => {
   await page.goto("https://www.saucedemo.com/");
-  await page.getByPlaceholder(/Username/).fill("locked_out_user");
-  await page.getByPlaceholder(/Password/).fill("secret_sauce");
-  await page.getByRole("button", { name: /Login/ }).click();
-  await expect(
-    page.getByText("Epic sadface: Sorry, this user has been locked out."),
-  ).toBeVisible();
+
+  for (const invalidUser of users.invalidUsers) {
+    await page.getByPlaceholder(/Username/).fill(invalidUser.username);
+    await page.getByPlaceholder(/Password/).fill(invalidUser.password);
+    await page.getByRole("button", { name: /Login/ }).click();
+    await expect(page.getByText(invalidUser.expectedError!)).toBeVisible();
+  }
 });
